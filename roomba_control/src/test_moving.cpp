a84f80846d3f41565c8762c2b4df0ff4ab44e3c0
pankratov.vs@gmail.com <pankratov.vs@gmail.com@fd06a06e-1be9-3ac1-c8d8-ef52681b3c63>
2012-06-08 21:53:20 +0000

    Added roomba_control

---
+++ roomba_control/src/test_moving.cpp
@@ -0,0 +1,111 @@
+#include "ros/ros.h"
+#include "geometry_msgs/Twist.h"
+
+#include <sstream>
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "test_moving");
+  ros::NodeHandle n;
+
+  ros::Publisher velocity_pub = n.advertise<geometry_msgs::Twist>("/cmd_vel", 1000);
+
+  ros::Rate rate(1);
+
+  if (ros::ok())
+  {
+    geometry_msgs::Twist msg;
+
+    msg.linear.x = 1.0; 
+    msg.angular.z = 0;
+    velocity_pub.publish(msg);
+
+    
+    ROS_INFO("%s", "Starting");
+
+    ros::spinOnce();
+
+    rate.sleep();
+  }
+
+  if (ros::ok())
+  {
+    geometry_msgs::Twist msg;
+
+    msg.linear.x = 0.2; 
+    msg.angular.z = 0;
+    velocity_pub.publish(msg);
+
+    
+    ROS_INFO("%s", "Forward");
+
+    ros::spinOnce();
+
+    rate.sleep();
+  }
+
+
+  if (ros::ok())
+  {
+    geometry_msgs::Twist msg;
+
+    msg.linear.x = -0.2; 
+    msg.angular.z = 0;
+    velocity_pub.publish(msg);
+
+    
+    ROS_INFO("%s", "Backward");
+
+    ros::spinOnce();
+
+    rate.sleep();
+  }
+  if (ros::ok())
+  {
+    geometry_msgs::Twist msg;
+
+    msg.linear.x = 0; 
+    msg.angular.z = 1.0;
+    velocity_pub.publish(msg);
+
+    
+    ROS_INFO("%s", "Left");
+
+    ros::spinOnce();
+
+    rate.sleep();
+  }
+  if (ros::ok())
+  {
+    geometry_msgs::Twist msg;
+
+    msg.linear.x = 0; 
+    msg.angular.z = -1.0;
+    velocity_pub.publish(msg);
+
+    
+    ROS_INFO("%s", "Right");
+
+    ros::spinOnce();
+
+    rate.sleep();
+  }
+  if (ros::ok())
+  {
+    geometry_msgs::Twist msg;
+
+    msg.linear.x = 0; 
+    msg.angular.z = 0;
+    velocity_pub.publish(msg);
+
+    
+    ROS_INFO("%s", "Stop");
+
+    ros::spinOnce();
+
+    rate.sleep();
+  }
+
+  return 0;
+}
+
